7c05dc2d66b1fd7f3641c0adc4c009d6909541c7
filipw <wojcieszyn@gmail.com>
2024-02-06 21:04:20 +0100

    added support for qir gen

--- qsharp-bridge/src/lib.rs
+++ qsharp-bridge/src/lib.rs
@@ -1,3 +1,5 @@
+mod tests;
+
 uniffi::include_scaffolding!("qsharp-bridge");
 
 use qsc::interpret::{Interpreter, self};
@@ -31,9 +33,7 @@ impl From<Vec<interpret::Error>> for QsError {
 }
 
 pub fn run_qs(source: &str) -> Result<ExecutionState, QsError> {
-
     let source_map = SourceMap::new(vec![("temp.qs".into(), source.into())], None);
-
     let mut interpreter = match Interpreter::new(
         true,
         source_map,
@@ -78,6 +78,25 @@ pub fn run_qs_shots(source: &str, shots: u32) -> Result<Vec<ExecutionState>, QsE
 
     return Ok(results);
 }
+
+pub fn qir(source: &str) -> Result<String, QsError> {
+    //let source_map = SourceMap::new(vec![("temp.qs".into(), source.into())], None);
+    let mut interpreter = match Interpreter::new(
+        true,
+        SourceMap::default(),
+        PackageType::Lib,
+        RuntimeCapabilityFlags::empty(),
+    ) {
+        Ok(interpreter) => interpreter,
+        Err(errors) => {
+            return Err(errors.into());
+        }
+    };
+
+    let result = interpreter.qirgen(source)?;
+    return Ok(result);
+}
+
 
 pub struct QubitState {
     id: String,
@@ -130,104 +149,5 @@ impl Receiver for ExecutionState {
     fn message(&mut self, msg: &str) -> Result<(), output::Error> {
         self.messages.push(msg.to_string());
         Ok(())
-    }
-}
-
-#[test]
-fn test_hello() {
-    let source = std::fs::read_to_string("tests/assets/hello.qs").unwrap();
-    let result = run_qs(&source).unwrap();
-
-    assert_eq!(result.messages.len(), 1);
-    assert_eq!(result.messages[0], "Hello");
-
-    assert_eq!(result.qubit_count, 0);
-    assert_eq!(result.states.len(), 0);
-
-    assert!(result.result.is_some());
-    assert_eq!(result.result, Some("()".into()));
-}
-
-#[test]
-fn test_hello_shots() {
-    let source = std::fs::read_to_string("tests/assets/hello.qs").unwrap();
-    let result = run_qs_shots(&source, 100).unwrap();
-
-    assert_eq!(result.len(), 100);
-    for res in &result {
-        assert_eq!(res.messages.len(), 1);
-        assert_eq!(res.messages[0], "Hello");
-    
-        assert_eq!(res.qubit_count, 0);
-        assert_eq!(res.states.len(), 0);
-    
-        assert!(res.result.is_some());
-        assert_eq!(res.result, Some("()".into()));
-    }
-}
-
-#[test]
-fn test_entanglement() {
-    let source = std::fs::read_to_string("tests/assets/entanglement.qs").unwrap();
-    let result = run_qs(&source).unwrap();
-
-    assert_eq!(result.messages.len(), 0);
-    assert_eq!(result.qubit_count, 2);
-    assert_eq!(result.states.len(), 2);
-
-    assert!(result.result.is_some());
-
-    let result_str = result.result.unwrap();
-    assert!(result_str == "(One, One)" || result_str == "(Zero, Zero)", "Unexpected result: {}", result_str);
-}
-
-#[test]
-fn test_entanglement_shots() {
-    let source = std::fs::read_to_string("tests/assets/entanglement.qs").unwrap();
-    let result = run_qs_shots(&source, 100).unwrap();
-
-    assert_eq!(result.len(), 100);
-
-    for res in &result {
-        assert_eq!(res.messages.len(), 0);
-        assert_eq!(res.qubit_count, 2);
-        assert_eq!(res.states.len(), 2);
-
-        assert!(res.result.is_some());
-
-        assert!(res.result == Some("(One, One)".into()) || res.result == Some("(Zero, Zero)".into()));
-    }
-}
-
-#[test]
-fn test_teleportation() {
-    let source = std::fs::read_to_string("tests/assets/teleportation.qs").unwrap();
-    let result = run_qs(&source).unwrap();
-
-    assert_eq!(result.messages.len(), 1);
-    assert_eq!(result.messages[0], "Teleported: true");
-
-    assert_eq!(result.qubit_count, 0);
-    assert_eq!(result.states.len(), 0);
-
-    assert_eq!(result.result, Some("true".into()));
-}
-
-#[test]
-fn test_teleportation_shots() {
-    let source = std::fs::read_to_string("tests/assets/teleportation.qs").unwrap();
-    let result = run_qs_shots(&source, 100).unwrap();
-
-    assert_eq!(result.len(), 100);
-
-    for inner_result in &result {
-
-        assert_eq!(inner_result.messages.len(), 1);
-        assert_eq!(inner_result.messages[0], "Teleported: true");
-
-        assert_eq!(inner_result.qubit_count, 0);
-        assert_eq!(inner_result.states.len(), 0);
-
-        assert_eq!(inner_result.result, Some("true".into()));
     }
 }
